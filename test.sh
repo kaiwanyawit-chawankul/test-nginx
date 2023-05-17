7afa3690ca4aa72580217c5f39132c1baef0741c
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2023-05-17 09:28:42 +0700

    fix test script

--- test.sh
+++ test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# curl -X POST -H "Content-Type: application/json" -d "@mock.json" http://localhost/setup/imposters
-curl -X GET http://localhost/api/swagger
-curl -X GET http://localhost/setup/imposters
-curl -X GET http://localhost/stub/api/feature1
+# Function to make a curl request and fail on error
+make_request() {
+    local url=$1
+
+    if ! curl -s --fail "$url"; then
+        echo "Failed to make a request to $url"
+        exit 1
+    fi
+}
+
+curl -X POST -H "Content-Type: application/json" -d "@mock.json" http://localhost/setup/imposters
+
+# Make curl requests and fail on error
+make_request http://localhost/api/swagger
+make_request http://localhost/setup/imposters
+make_request http://localhost/stub/api/feature1
+
+exit 0
