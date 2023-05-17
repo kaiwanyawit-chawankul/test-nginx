d142a7479a46340a875464087b0549ba695e1a5e
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2023-05-17 09:12:24 +0700

    add test

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
-docker build . -t nginx:job
-#docker run -p 127.0.0.1:80:80 nginx:job
-docker run -p 127.0.0.1:8080:80 nginx:job
+#!/bin/bash
 
-docker run -it --rm -p 8080:80 --name web nginx
-
-docker run -it --rm -p 80:80 --name web nginx:alpine
-docker run -it --rm -p 2525:2525 --name stub bbyars/mountebank:latest
+curl -X POST -H "Content-Type: application/json" -d "@mock.json" http://localhost/setup/imposters
+curl -X GET http://localhost/api/swagger
+curl -X GET http://localhost/setup/imposters
+curl -X GET http://localhost/stub/api/feature1
