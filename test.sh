983662b45e7113e5b461bef2b64639591233aabe
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2023-05-17 09:16:42 +0700

    check fail pipeline

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" -d "@mock.json" http://localhost/setup/imposters
+# curl -X POST -H "Content-Type: application/json" -d "@mock.json" http://localhost/setup/imposters
 curl -X GET http://localhost/api/swagger
 curl -X GET http://localhost/setup/imposters
 curl -X GET http://localhost/stub/api/feature1
